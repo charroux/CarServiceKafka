ce21a2256e1000558a654a480a7d51d31962f3d5
charroux <benoit.charroux@gmail.com>
2022-12-10 18:34:27 +0100

    first commit

---
+++ producer/src/main/java/com/example/producer/CarEvent.java
@@ -0,0 +1,13 @@
+package com.example.producer;
+
+public class CarEvent {
+    String plateNumber;
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+}
---
+++ producer/src/main/java/com/example/producer/CarEventSerializer.java
@@ -0,0 +1,41 @@
+package com.example.producer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.serialization.Serializer;
+
+import java.util.Map;
+
+public class CarEventSerializer implements Serializer<CarEvent> {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+        Serializer.super.configure(configs, isKey);
+    }
+
+    @Override
+    public byte[] serialize(String topic, CarEvent data) {
+        try {
+            if (data == null){
+                System.out.println("Null received at serializing");
+                return null;
+            }
+            return objectMapper.writeValueAsBytes(data);
+        } catch (Exception e) {
+            throw new SerializationException("Error when serializing MessageDto to byte[]");
+        }
+    }
+
+    @Override
+    public byte[] serialize(String topic, Headers headers, CarEvent data) {
+        return Serializer.super.serialize(topic, headers, data);
+    }
+
+    @Override
+    public void close() {
+        Serializer.super.close();
+    }
+}
